8af7f4314d333dd277964b05406203a5f00f1813
nicohan3134 <nicolas26hanna2020@gmail.com>
2026-04-22 19:00:31 -0400

    Update WN11-CC-000110.ps1

--- STIGS/WN11-CC-000110.ps1
+++ STIGS/WN11-CC-000110.ps1
@@ -1,4 +1,16 @@
+.SYNOPSIS
+   This PowerShell script disables the ability to print over HTTP, preventing the client from sending print jobs to internet-based printers.
 
+.NOTES
+    Author          : Nicolas Hanna
+    LinkedIn        : linkedin.com/in/nicolas--hanna/
+    GitHub          : github.com/nicohan3134
+    Date Created    : 2026-22-04
+    Last Modified   : 2026-22-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000500
 
 # Blocks the client from printing to internet-based printers over HTTP
 $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers"
